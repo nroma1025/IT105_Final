20ca643eabba98cc98429aa307276d070f395f10
nroma1025 <63817818+nroma1025@users.noreply.github.com>
2020-04-16 16:51:12 -0400

    initial commit

---
+++ ProjectHelperV1.py
@@ -0,0 +1,70 @@
+import random
+
+def CheatSheet():
+    playPoints = ['Pair-2', 'Three of a Kind-3', 'Four of a Kind-4', 'Nonconsecutive series-# cards in series', 'Sum to 15-2', 'Sum to 31-2', 'Last Card-1', 'Nobs-2']
+    handPoints = ['Pair-2', 'Three of a Kind-6', 'Four of a Kind-12', 'Series-# cards in series', 'Sum to 15-2', 'Four card flush-4', 'Five card flush-5', 'Nibs-1']
+    print("Points can be earned during play like so: ")
+    for i in range(0,len(playPoints)):
+        print(playPoints[i])
+    print("Points can be earned in the hand and crib like so: ")
+    for i in range(0,len(handPoints)):
+        print(handPoints[i])
+    return()
+  
+def Instructions():
+    print("The objective of the game is to be the first to accrue 121 points")
+    print("The dealer alternates between rounds. Each round, players will be dealt a six-card hand.")
+    print("Players will discard two cards to 'The Crib'. The crib is an additional four-card hand for the dealer.")
+    print("One card is dealt as a community card before play begins. Players discard before the community card is revealed.")
+    print("Players will play cards, one at a time, during the 'play'. Points will be given during the play for the following: ")
+    continuePrompt = False
+    while continuePrompt == False:    
+        continuePrompt = input("Press 'y' to continue: ")
+        if continuePrompt == 'y':
+            continuePrompt = True
+        else:
+            continuePrompt = False
+    playPoints = ['Pair-2', 'Three of a Kind-3', 'Four of a Kind-4', 'Nonconsecutive series-# cards in series', 'Sum to 15-2', 'Sum to 31-2', 'Last Card-1', 'Nobs-2']
+    for i in range(0,len(playPoints)):
+        print(playPoints[i])
+    continuePrompt = False
+    while continuePrompt == False:    
+        continuePrompt = input("Press 'y' to continue: ")
+        if continuePrompt == 'y':
+            continuePrompt = True
+        else:
+            continuePrompt = False
+    print("As each player puts a card down, he/she adds his/her card's value to a running total. The total cannot go over 31.")
+    print("If a player is unable to play a card without going over 31, they call 'Go'.")
+    print("The other player will continue to play cards until they, too, are unable to stay under 31.")
+    print("At this point, the players start a new total from 0 and play until all cards are played.")
+    print("The play is done, and all points are added to the player's total. Next comes scoring the hand")
+    print("Players will attempt to make the best 5-card hand with their hand and the community card.")
+    print("The non-dealer scores his/her hand first. Points are awarded as follows: ")
+    continuePrompt = False
+    while continuePrompt == False:    
+        continuePrompt = input("Press 'y' to continue: ")
+        if continuePrompt == 'y':
+            continuePrompt = True
+        else:
+            continuePrompt = False
+    handPoints = ['Pair-2', 'Three of a Kind-6', 'Four of a Kind-12', 'Series-# cards in series', 'Sum to 15-2', 'Four card flush-4', 'Five card flush-5', 'Nibs-1']
+    for i in range(0,len(handPoints)):
+        print(handPoints[i])
+    continuePrompt = False
+    while continuePrompt == False:    
+        continuePrompt = input("Press 'y' to continue: ")
+        if continuePrompt == 'y':
+            continuePrompt = True
+        else:
+            continuePrompt = False
+    print("After the dealer counts his/her hand, he/she will count their crib as an additional hand.")
+    print("After these three scoring phases, the deck will reshuffle and re-deal, and the dealer will switch.")
+    #print("Players can bring up the score cheat sheet with XXX")
+    print("Some notes: Nobs is when a jack is revealed as the community card. Points go to the dealer.")
+    print("Nibs is when a jack in your hand is the same suit as the community card.")
+    print("You cannot get points for summing to 31 AND getting the last card. 31 automatically implies a last card.")
+    print("Series during play does not have to be consecutive. IE, 3-5-4 is a valid 3-card series.")
+    print("Face cards are worth 10, Aces are worth 1.")
+    print("Finally, you get points for each unique series. A hand of 3-3-4-5 is therefore 2 unique '3-4-5' series.")
+    print("Now you're ready to play!")
---
+++ project rough v1.py
@@ -0,0 +1,110 @@
+#works cited
+#https://pydealer.readthedocs.io/en/latest/usage.html#construct-a-deck
+
+#To Do
+# - DONE - Describe rules of play
+# - DONE - make a cheat sheet for hands
+# make a program to count the user's hand
+# make a program to count the computer's hand
+# make a program to discard from the user and the computer to the crib
+# make a program to count points from the play
+# - DONE (-ISH?) - make a program to reshuffle and deal the deck
+#thoughts: have a list, append each time a card is played. compare ranks to detect for non-con series?
+#thoughts: have a whole separate module to check for scoring conditions?
+#thoughts: make a roll check to see if the computer will forget to count points based on difficulty
+# - DONE - Stage: Intro
+# - DONE - Stage: Deal
+# Stage: Discard
+# Stage: Play
+# Stage: Hand
+# Stage: Crib
+# Stage: End Round
+
+
+import pydealer
+import ProjectHelperV1
+import random
+
+# Stage: Intro
+introChoice = input("Welcome to Cribbage! Would you like a description of the rules? [y/n]")
+while introChoice != 'y' and introChoice != 'n':
+    print("Not today, ISIS. Try a valid input.")
+    introChoice = input("Welcome to Cribbage! Would you like a description of the rules? [y/n]")
+if introChoice == 'y':
+    ProjectHelperV1.Instructions()
+elif introChoice == 'n':
+    print("You're ready to play!")
+
+# Construct a Deck instance, with 52 cards.
+deck = pydealer.Deck()
+#initialize some variables
+playerScore = 0
+comScore = 0
+playerDealer = 1
+while playerScore < 121 and comScore < 121:
+    # Stage: Deal
+    print("Player deals first.")
+    #make sure you set up something like a modulo. odd numbers, player deals. even, computer deals. +=1 after every round
+    deck.shuffle(2)
+    playerHand = deck.deal(6)
+    comHand = deck.deal(6)
+    cribHand = []
+    
+    # Stage: Discard
+    # ATTN: Input cheat sheet option here somewhere
+    discardBool = False
+    while discardBool == False:
+        for i in range(0,len(playerHand)):
+            print(playerHand[i])
+        discard1=int(input("Choose a card, 1-6, to send the the crib."))
+        if discard1 > 6 or discard1 < 1:
+            print('Try again.')
+        else:
+            discardBool = True    
+    
+    discard1 = int(discard1 - 1)
+    cribHand.append(playerHand[discard1])
+    del playerHand[discard1]
+
+    #@O'Shea: this is the issue below
+    #For whatever reason, this loop doesn't kick in
+    #The program never prompts the user for a second discard choice
+    discardBool == False
+    while discardBool == False:   
+        for i in range(0,len(playerHand)):
+            print(playerHand[i])
+        discard2=int(input("Choose a card, 1-5, to send the the crib."))
+        if discard2 > 5 or discard2 < 1:
+            print('Try again.')
+        else:
+            discardBool = True 
+    # discardBool == False
+    # discardStr = playerHand[discard1]
+    # cribHand.append(discardStr)
+    # playerHand.remove(discardStr)
+
+    discard2 = int(discard2 - 1)
+    cribHand.append(playerHand[discard2])
+    del playerHand[discard2]
+    
+    # discard1 = int(random.randint(0,5))
+    # discardStr = comHand[discard1]
+    # cribHand.append(discardStr)
+    # del comHand[discard1]
+    # discard1 = int(random.randint(0,4))
+    # discardStr = comHand[discard1]
+    # cribHand.append(discardStr)
+    # del comHand[discard1]
+
+    print("Your hand: ")
+    for i in range(0,len(playerHand)):
+        print(playerHand[i])
+    print('')
+    print("Computuer's hand: ")
+    for i in range(0,len(comHand)):
+        print(comHand[i])
+    print('')
+    print('Crib: ')
+    for i in range(0,len(cribHand)):
+        print(cribHand[i])
+    comScore = 122
